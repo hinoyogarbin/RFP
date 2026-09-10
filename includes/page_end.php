c00934ae1e20cb8e94314a64be596d482393df99
hinoyogarbin <hinoyogarvin@gmail.com>
2026-09-10 22:37:11 +0800

    Create development branch

--- includes/page_end.php
+++
@@ -1,4 +0,0 @@
-</main>
-<?= $extraScripts ?? '' ?>
-</body>
-</html>
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/auth_check.php';
-
-if (empty($_SESSION['user_id'])) {
-    header('Location: auth/login.php');
-    exit;
-}
-
-redirectToDashboard($_SESSION['role']);
